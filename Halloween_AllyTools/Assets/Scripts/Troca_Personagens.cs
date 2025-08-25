081b0cdb0fdf546525424030a349e303d68b198c
Yuri Scheunemann <yuri.scheunemann@alunos.sc.senac.br>
2025-08-25 11:18:36 -0300

    Script de trocar personagem feito

--- Halloween_AllyTools/Assets/Scripts/Troca_Personagens.cs
+++ Halloween_AllyTools/Assets/Scripts/Troca_Personagens.cs
@@ -2,21 +2,58 @@ using UnityEngine;
 
 public class Troca_Personagens : MonoBehaviour
 {
-    private bool swapersonInput;
-    
-    void Start()
-    {
-        swapersonInput = Input.GetKeyDown(KeyCode.B);
-    }
+    public bool lobsomen_player = false;
+    public bool zombie_player = false;
+    public bool human_player = true;
 
-    
+    public GameObject Lobisomen_Object;
+    public GameObject Zombie_Object;
+    public GameObject Human_Object;
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            SwapLobi();
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            SwapZombie();
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            SwapHuman();
+        }
     }
 
-    public void Swap()
+    public void SwapLobi()
     {
+        lobsomen_player = true;
+        zombie_player = false;
+        human_player = false;
+        Update_Objects();
+    }
 
+    public void SwapZombie()
+    {
+        lobsomen_player = false;
+        zombie_player = true;
+        human_player = false;
+        Update_Objects();
+    }
+
+    public void SwapHuman()
+    {
+        lobsomen_player = false;
+        zombie_player = false;
+        human_player = true;
+        Update_Objects();
+    }
+
+    void Update_Objects()
+    {
+        Lobisomen_Object.SetActive(lobsomen_player);
+        Zombie_Object.SetActive(zombie_player);
+        Human_Object.SetActive(human_player);
     }
 }
