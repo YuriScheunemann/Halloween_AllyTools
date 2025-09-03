61a9172d4af55a31cb7d2d3d6d1e14fb6e2d61e0
BenjamimThomas <benjamim.silva1@alunos.sc.senac.br>
2025-09-03 14:37:31 -0300

    som habilidades
    Cavar, arranhar (lobisomem)

--- Halloween_AllyTools/Assets/Scripts/Zombie_Hability.cs
+++ Halloween_AllyTools/Assets/Scripts/Zombie_Hability.cs
@@ -7,34 +7,58 @@ public class Zombie_Hability : MonoBehaviour
     public LayerMask buraco;
     public Transform ponto_Teleporte;
     public Transform buraco_Teleporte;
+
+    public AudioClip somHabilidade; 
+    private AudioSource audioSource;
+
     Troca_Personagens troca;
+
     void Start()
     {
         troca = FindObjectOfType(typeof(Troca_Personagens)) as Troca_Personagens;
+        audioSource = GetComponent<AudioSource>();
 
+        
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
     }
 
     void Update()
-    {        
+    {
         if (Input.GetKeyDown(KeyCode.E) && troca.Zombie_Object.activeSelf)
         {
             Hability();
         }
     }
-    
+
     void Hability()
-    {    
-        Collider2D colisor = Physics2D.OverlapCircle(troca.Personagens_Object.transform.position, 0.1f, buraco);
+    {
+        Collider2D colisor = Physics2D.OverlapCircle(
+            troca.Personagens_Object.transform.position,
+            0.1f,
+            buraco
+        );
 
         if (colisor != null && ponto_Teleporte != null && troca.Zombie_Object.activeSelf)
         {
             troca.Personagens_Object.transform.position = buraco_Teleporte.position;
+            TocarSom();
         }
-        
+
         if (colisor != null && buraco_Teleporte != null && troca.Zombie_Object.activeSelf)
-        {           
+        {
             troca.Personagens_Object.transform.position = ponto_Teleporte.position;
-        }        
+            TocarSom();
+        }
     }
-   
+
+    void TocarSom()
+    {
+        if (somHabilidade != null)
+        {
+            audioSource.PlayOneShot(somHabilidade);
+        }
+    }
 }
