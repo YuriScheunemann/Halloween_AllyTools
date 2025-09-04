172f47d15ff4158519c7205f0d2fa17ae5d4fdd9
Yuri Scheunemann <yuri.scheunemann@alunos.sc.senac.br>
2025-09-03 22:06:16 -0300

    Finish_Game

--- Halloween_AllyTools/Assets/Scripts/Zombie_Hability.cs
+++ Halloween_AllyTools/Assets/Scripts/Zombie_Hability.cs
@@ -33,19 +33,13 @@ public class Zombie_Hability : MonoBehaviour
 
     void Hability()
     {
-        Collider2D colisor = Physics2D.OverlapCircle(troca.Personagens_Object.transform.position,0.1f,buraco);
+        Collider2D colisor = Physics2D.OverlapCircle(troca.Personagens_Object.transform.position,1f,buraco);
 
         if (colisor != null && ponto_Teleporte != null && troca.Zombie_Object.activeSelf)
         {
             troca.Personagens_Object.transform.position = buraco_Teleporte.position;
             TocarSom();
-        }
-
-        if (colisor != null && buraco_Teleporte != null && troca.Zombie_Object.activeSelf)
-        {
-            troca.Personagens_Object.transform.position = ponto_Teleporte.position;
-            TocarSom();
-        }
+        }       
     }
 
     void TocarSom()
